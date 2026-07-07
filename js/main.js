872b6cd74ff94e756ec5edd5cb8f3b018e6863dd
David Vargas <david.vargas@ppm.com.ec>
2026-07-06 19:03:21 -0500

    Adicion de comentarios y eliminacion de redundancia en el codigo

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-const button = document.querySelector('button');
-
+/* FUncion para abrir el modal de los shows seleccionados  */
 function openModal(show) {
     const modalBody = document.getElementById("modal-body");
+     /* creacion del html del modal para agilizar el desarrollo */
     modalBody.innerHTML = `
         <h2>${show.name}</h2>
         <img src="${show.image?.original}" class = "modal-img" alt="${show.name}">
@@ -14,19 +14,26 @@ function openModal(show) {
 }
 
 const createShowCard = (show) => {
+    /* Crea al div contenedor y le agrega la clase show-card */
     const card = document.createElement("div");
     card.classList.add("show-card");
+    /* Crea el elemento de imagen */
     const image = document.createElement("img");
+    /* Agrega la clase show-image al elemento de imagen */
     image.classList.add("show-image");
     image.src = show.image?.medium || "default-image.jpg";
     image.alt = show.name;
+    /* Crea el div contenedor de la informacion del show */
     const info = document.createElement("div");
     info.classList.add("show-info");
+    /*/ Agrega el nombre del show y los generos al contenedor de informacion */
     const name = document.createElement("h2");
     name.classList.add("show-name");
     name.textContent = show.name;
+    /* Crea el contenedor para los géneros */
     const genres = document.createElement("div");
     genres.classList.add("show-genres");
+    /* Agrega cada género como un span dentro del contenedor de géneros */
     show.genres.forEach((genre)=>{
         const span = document.createElement("span");
         span.classList.add(
@@ -45,12 +52,10 @@ const createShowCard = (show) => {
         const response = await axios.get(`https://api.tvmaze.com/shows/${show.id}`);
         openModal(response.data);
     });
-    
-    return card;
     return card;
 };
 
-
+/* Espera a que el DOM este completamente cargado antes de ejecutar el codigo */
 document.addEventListener('DOMContentLoaded', async () => {
     const contenedor = document.getElementsByClassName("contenedor")[0];
     try{
@@ -66,7 +71,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     }
 });
 
-
+/* Funcion para cerrar el modal */
 const closeModalButton = document.getElementsByClassName("close-modal")[0];
 closeModalButton.addEventListener("click", () => {
     document.getElementById("modal").classList.add("hidden");
